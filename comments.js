875b9fd2b85320f9f21b371d819a39ef4fb06b74
Mirelly Almeida <136247614+Mihvieira@users.noreply.github.com>
2024-12-06 20:37:19 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -4,3 +4,28 @@
 // create a route for comments/:id
 // create a route for comments/:id/edit
 // create a route for comments/:id/delete
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Comments');
+});
+
+router.get('/new', (req, res) => {
+    res.send('New Comment');
+});
+
+router.get('/:id', (req, res) => {
+    res.send('Comment ' + req.params.id);
+});
+
+router.get('/:id/edit', (req, res) => {
+    res.send('Edit Comment ' + req.params.id);
+});
+
+router.get('/:id/delete', (req, res) => {
+    res.send('Delete Comment ' + req.params.id);
+});
+
+module.exports = router;
